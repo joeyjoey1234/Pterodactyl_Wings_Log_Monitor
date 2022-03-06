6f887ec21a43c1fc8cff027e1c09fc1cbeab1571
joeyjoey1234 <37442571+joeyjoey1234@users.noreply.github.com>
2022-03-05 17:35:23 -0700

    Update main.py
    added machine name option for email subject line.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from_address = '' ###Example:  Cool Server <servername@cooldomain.com>
 to_address = '' ### Example: 'cool dude <coolguy@example.com>'
 api_key = '' ## Example: 'kdjfalsdjflaksfkjadl;kfjlsdfk'
 post_url = '' ##### https://api.mailgun.net/v3/sandbox911560439eda4a10bc2c5361f5f55dd2.mailgun.org/messages
+machine_name = '' ### Machine name that will be in the email subject line.
 
 
 clear = 'echo " " > /var/log/pterodactyl/wings.log'
@@ -24,14 +25,14 @@ def send_simple_message(message,subjec):
 
 def process(x):
     if 'ERROR' in x:
-        send_simple_message(x,'Error on Atomic')
+        send_simple_message(x,'Error on {}'.format(machine_name))
         print('sending error')
     elif 'crashed' in x:
-        send_simple_message(x, 'Container Crash on Atomic')
+        send_simple_message(x, 'Container Crash on {}'.format(machine_name))
         print('sending Crash email')
     elif 'stacktrace' in x:
-        send_simple_message(x, 'Stacktrace on Nitro ')
-        print('sending Crash email')
+        send_simple_message(x, 'Stacktrace on {}'.format(machine_name))
+        print('sending Stracktrace email')
     else:
         pass
 
